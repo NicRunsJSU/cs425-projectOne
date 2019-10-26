6888fb27646582e928d58dc7eacbe59a527fff5b
Nic Morrison <nmorrison@stu.jsu.edu>
2019-10-26 18:56:42 -0500

    cleaned up comments because they began to frustrate me.

--- src/java/edu/jsu/mcis/cs425/project1/registration.java
+++ src/java/edu/jsu/mcis/cs425/project1/registration.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package edu.jsu.mcis.cs425.project1;
 
 import java.io.IOException;
@@ -19,15 +15,10 @@ import java.sql.*;
 @WebServlet(name = "registration", urlPatterns =  {"/registration"})
 public class registration extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+
+    
+    
+    
     
     
     
@@ -49,49 +40,36 @@ public class registration extends HttpServlet {
         }
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+       
+    
+    
+    
+    
+    
+    
     @Override
+    @SuppressWarnings("empty-statement")
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        Class.forName("com.mysql.jdbc.Driver").newInstance();
-        
-        String url= "jdbc:mysql://localhost/registration_db";
-        
-        Connection conn = DriverManager.getConnection(url, root, CS425);
-        
-        
-        
-        
-        processRequest(request, response);
+    
+   
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+
+    
+    
+    
+    
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
+
+    
+    
+    
     @Override
     public String getServletInfo() {
         return "Short description";
