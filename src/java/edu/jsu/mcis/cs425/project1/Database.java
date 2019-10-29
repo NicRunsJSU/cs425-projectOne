085c5a1f1a91ace89d5bdbf749a1ac4c1c549ae2
Nic Morrison <nmorrison@stu.jsu.edu>
2019-10-29 14:16:37 -0500

    Finished up the Database getQueryResults function in the Database.java.

--- src/java/edu/jsu/mcis/cs425/project1/Database.java
+++ src/java/edu/jsu/mcis/cs425/project1/Database.java
@@ -4,14 +4,20 @@ import java.sql.ResultSetMetaData;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.List;
+import org.json.simple.JSONArray;
 
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 
 public class Database {
+    
+    String sessID;
     
     private Connection getConnection() {
         
@@ -25,10 +31,56 @@ public class Database {
             conn = ds.getConnection();
             
         }        
-        catch (Exception e) { e.printStackTrace(); }
+        catch (Exception stinkystinky) { stinkystinky.printStackTrace(); }
         
         return conn;
 
+    }
+    
+    public String getQueryResults(String sessionID) throws SQLException {
+    
+        
+        StringBuilder tbl = new StringBuilder();
+        String query;
+        
+        this.sessID = sessionID;
+        Connection defcon = getConnection();
+        
+        query = "SELECT * FROM registrations r WHERE sessionid = ?;";
+        
+        try { 
+            PreparedStatement statement = defcon.prepareStatement(query);
+            statement.setString(0, sessID);
+            
+            if (statement.execute()) {
+                ResultSet result = statement.getResultSet();
+                
+                //  MAKE TABLE
+                tbl.append("<table>");
+                
+                //POPULATE TABLE
+                
+                while (result.next()){
+                    
+                    tbl.append("<tr>");
+                    tbl.append("<td>").append(result.getString("id")).append("</td>");
+                    tbl.append("<td>").append(result.getString("firstname")).append("</td>");
+                    tbl.append("<td>").append(result.getString("lastname")).append("</td>");
+                    tbl.append("<td>").append(result.getString("displayname")).append("</td>");
+                    tbl.append("<td>").append(sessionID).append("</td>");
+                    tbl.append("</tr>");
+                    
+                }
+                
+                tbl.append("</table>");
+                     
+                
+            }
+            
+            
+        }
+        catch(Exception blahblahblah){System.err.println(blahblahblah);}
+        return (tbl.toString());
     }
     
 }
